66b0faa581693e2129fb8f06e51578453cea8fdd
babburik <75203250+babburik@users.noreply.github.com>
2020-11-29 16:54:34 +0530

    adding ne wfil from repo

---
+++ demo3.java
@@ -0,0 +1,9 @@
+class demo1
+{
+   public static void main(String args[]){
+	   
+	   System.out.println("this is devops demo 2nd class");
+	   System.out.println("hallow krishna");
+	   System.out.println("hallow krishna babburi  demo 3 class");
+   }
+   }
